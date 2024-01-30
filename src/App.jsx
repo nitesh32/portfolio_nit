97380f7312a393e8942a046dbf8de9ae07518efc
nitesh32 <nit8339@gmail.com>
2024-01-30 11:55:27 +0530

    started portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black-300  border p-4 m-4 rounded hover:border-green-500 cursor-pointer select-none">
+        Upcoming......
+      </div>
+    </>
+  );
+}
+
+export default App;
