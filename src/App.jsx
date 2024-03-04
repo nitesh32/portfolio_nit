84d0a3a665941e3da6abdd1f9184d0b50d2b7422
nitesh32 <nit8339@gmail.com>
2024-03-04 22:50:28 +0530

    adding favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import "./App.css";
+import { useState } from "react";
+import "./App.css"
+import {motion} from 'framer-motion'
 
 function App() {
+  const [card,setcard]=useState(false);
   return (
     <>
+
       <div className="bg-black-300  border p-4 m-4 rounded hover:border-green-500 cursor-pointer select-none">
         Upcoming......
       </div>
@@ -10,4 +14,9 @@ function App() {
   );
 }
 
+
+
 export default App;
+
+
+
